fa5b4c61b28c0f27b0b87a2c88bd6f80f558c67d
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-09-03 23:24:57 +0200

    Refactor book addition functionality to use input values and dynamically create book cards

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const myLibrary = [];
 
-function Book(title, author, pages, isRead, unId) {
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const isRead = document.querySelector("#isRead");
+const addBookBtn = document.querySelector(".addBtn");
+
+const cardsContainer = document.querySelector(".cards-container");
+
+
+function Book(title, author, pages, isRead, unId,) {
     if (!new.target) {
         throw Error("new is missing from the constructor")
     }
@@ -20,13 +29,63 @@ function Book(title, author, pages, isRead, unId) {
 }
 
 function addBookToLibrary() {
-    let newBook = new Book("rich", "nkateko", 233, false, "hashdh999")
-    myLibrary.push(newBook)
+  let newBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    isRead.checked,
+    crypto.randomUUID()
+  );
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+
+  // Create and append card ONLY for the new book
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.setAttribute("data-id", newBook.unId);
+
+  card.innerHTML = `<div class="card__title">${newBook.title}</div>
+  <button id="delete_btn">X</button>
+       <div class="card__title">${newBook.author}</div>
+       <div class="card__subtitle">
+         ${newBook.info()}
+       </div>
+       <div class="card__indicator">
+         <span class="card__indicator-amount">135</span> Works /
+         <span class="card__indicator-percentage">${newBook.pages}</span>
+       </div>`;
+
+  cardsContainer.appendChild(card);
 }
 
-addBookToLibrary()
-console.log(myLibrary)
 
-myLibrary.map((book) => {
-    console.log(book.title)
+
+
+addBookBtn.addEventListener("click", () => {
+    addBookToLibrary()
+   title.value = "";
+   author.value = "";
+   pages.value ="";
+   isRead.checked = false;
+})
+
+let deleteBtn = document.querySelector("#delete_btn");
+
+deleteBtn.addEventListener((e) => {
+  if (e.target.classList.contains("delete_btn")) {
+    const card = e.target.closest(".card");
+    const unId = card.getAttribute("data-id");
+
+    // Remove from DOM
+    card.remove();
+
+    // Remove from library array
+    const index = myLibrary.findIndex((book) => book.unId === unId);
+    if (index !== -1) {
+      myLibrary.splice(index, 1);
+    }
+
+    console.log("Deleted book with ID:", unId);
+    console.log("Updated Library:", myLibrary);
+  }
 })
